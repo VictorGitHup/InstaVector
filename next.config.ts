74325e3ad22648a702f12eff5ae06b0e4652ca1a
Victor Botina Jojoa <victorabj@gmail.com>
2025-10-07 20:16:04 +0000

    remplaza la url de la api con https://convertsvg-fclo.onrender.com/conve

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_URL: 'https://convertsvgnw.onrender.com/convert/',
+    NEXT_PUBLIC_API_URL: 'https://convertsvg-fclo.onrender.com/convert/',
   },
   typescript: {
     ignoreBuildErrors: true,
