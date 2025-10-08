1594e78b1f896b0a5794145cf1b6030d042dbf1b
Victor Botina Jojoa <victorabj@gmail.com>
2025-10-08 16:52:21 +0000

    al hacer el cambio el icono se desborda, como solucionarlo?

--- src/components/icono.tsx
+++ src/components/icono.tsx
@@ -17,6 +17,10 @@ const Icono: React.FC<IconoProps> = (props) => {
         const svgDoc = parser.parseFromString(text, 'image/svg+xml');
         const svgElement = svgDoc.getElementsByTagName('svg')[0];
         if (svgElement) {
+          // Remove default width and height to allow Tailwind classes to take over
+          svgElement.removeAttribute('width');
+          svgElement.removeAttribute('height');
+
           // Ensure all paths use currentColor for fill
           svgElement.querySelectorAll('path').forEach(path => {
             path.setAttribute('fill', 'currentColor');
@@ -31,6 +35,7 @@ const Icono: React.FC<IconoProps> = (props) => {
     return <div style={{ width: 24, height: 24 }} />; // Placeholder while loading
   }
 
+  // Pass the props to a div that will contain the SVG
   return <div {...props} dangerouslySetInnerHTML={{ __html: svgContent }} />;
 };
 
